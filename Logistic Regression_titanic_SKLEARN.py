c098c11c6793573de60e058ae73fe5d6f5abe2a2
Henrique <101234830+HOR93@users.noreply.github.com>
2024-10-07 00:46:42 -0300

    Update Logistic Regression_titanic_SKLEARN.py

--- Logistic Regression_titanic_SKLEARN.py
+++ Logistic Regression_titanic_SKLEARN.py
@@ -98,3 +98,29 @@ from sklearn.metrics import classification_report
 
 print(classification_report(y_test,predict))
 
+""" 
+A matriz de confusão mostra as previsões do modelo em relação aos valores reais. Aqui está a interpretação para a matriz de confusão fornecida:
+
+True Negative (TN): 151 - O modelo previu corretamente 151 casos como "Não sobreviventes" quando eles realmente não sobreviveram.
+False Positive (FP): 24 - O modelo previu incorretamente 24 casos como "Sobreviventes" quando eles realmente não sobreviveram.
+False Negative (FN): 37 - O modelo previu incorretamente 37 casos como "Não sobreviventes" quando eles realmente sobreviveram.
+True Positive (TP): 83 - O modelo previu corretamente 83 casos como "Sobreviventes" quando eles realmente sobreviveram.
+
+
+Precision: A precisão é a proporção de verdadeiros positivos (TP) em relação ao total de previsões positivas (TP + FP). Para a classe 0 (Não sobrevivente), a precisão é de 0.80 e para a classe 1 (Sobrevivente), a precisão é de 0.78. Isso significa que o modelo tem uma precisão de 80% para prever corretamente os casos de "Não sobrevivente" e 78% para prever corretamente os casos de "Sobrevivente".
+
+Recall: O recall (ou taxa de verdadeiros positivos) é a proporção de verdadeiros positivos (TP) em relação ao total de valores reais positivos (TP + FN). Para a classe 0, o recall é de 0.86 e para a classe 1, o recall é de 0.69. Isso significa que o modelo captura corretamente 86% dos casos de "Não sobrevivente" e 69% dos casos de "Sobrevivente".
+
+F1-score: O F1-score é a média harmônica da precisão e do recall. Ele fornece uma medida única que equilibra precisão e recall. Para a classe 0, o F1-score é de 0.83 e para a classe 1, o F1-score é de 0.73.
+
+Support: O número de ocorrências de cada classe no conjunto de teste.
+
+Accuracy: A precisão geral do modelo em prever corretamente as classes. Neste caso, é de 0.79, o que significa que o modelo previu corretamente cerca de 79% dos casos no conjunto de teste.
+
+Macro avg: A média simples das métricas (precisão, recall, F1-score) para todas as classes.
+
+Weighted avg: A média das métricas (precisão, recall, F1-score) para todas as classes, ponderada pelo suporte (número de ocorrências de cada classe).
+
+Globalmente, o modelo parece ter um desempenho razoável, com uma precisão, recall e F1-score em torno de 0.79 para ambas as classes.
+
+"""
